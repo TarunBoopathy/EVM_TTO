344847939128e2d8ef1eacfba08a54e6ce97d480
TarunBoopathy <97330830+TarunBoopathy@users.noreply.github.com>
2025-09-26 12:12:41 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_evm (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,30 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uio_out = 0;
   assign uio_oe  = 0;
 
+    evm dut(
+        .clk(clk),
+        .rst(rst_n),
+        .vote_candidate_1(ui_in[0]),
+        .vote_candidate_2(ui_in[1]),
+        .vote_candidate_3(ui_in[2]),
+        .switch_on_evm(ui_in[3]),
+        .candidate_ready(ui_in[4]),
+        .voting_session_done(ui_in[5]),
+        .switch_off_evm(ui_in[6]),
+        .display_winner(ui_in[7]),
+        .display_results(uio_in[1:0]),
+        .candidate_name(uo_out[1:0]),
+        .invalid_results(uo_out[2]),
+        .results(uio_out[6:0]),
+        .voting_in_progress(uo_out[3]),
+        .voting_done(uo_out[4])
+    );  
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
