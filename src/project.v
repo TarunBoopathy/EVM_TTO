6beba61dd96a1d3cd9fa9a6668c1acf415596035
TarunBoopathy <97330830+TarunBoopathy@users.noreply.github.com>
2025-09-26 12:29:18 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,8 +18,11 @@ module tt_um_evm (
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  //assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uio_out[7] = 0;
+    assign uo_out[5] = 0;
+    assign uo_out[6] = 0;
+    assign uo_out[7] = 0;
+    assign uio_oe  = 0;
 
     evm dut(
         .clk(clk),
@@ -41,6 +44,6 @@ module tt_um_evm (
     );  
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, 1'b0};
+    wire _unused = &{ena, uio[7:2], 1'b0};
 
 endmodule
